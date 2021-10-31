aa8c4abffa05030b6dea50041dd8506adc1cc5be
Anto Wiranto <91955881+antowirantoIO@users.noreply.github.com>
2021-10-31 13:02:34 +0700

    initial commit

---
+++ LK8-03-AntoWiranto/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>")
+document.writeln("<hr width=600 color=black>")
+document.writeln("<h1>SMK NEGERI 1 CIAMIS</h1>")
+document.writeln("<h2>TEKNIK KOMPUTER DAN INFORMATIKA</h2>")
+document.writeln("<h3>JURUSAN REKAYASA PERANGKAT LUNAK</h3>")
+document.writeln("<hr width=600 color=black")
+document.writeln("</center>")
